33486bcbbe05ccc55767cb67552351205a6d3e16
+ <mcrea23@aol.com>
2021-01-04 16:46:05 -0600

    had to start project over

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,41 @@
 require './config/environment'
-
 class ApplicationController < Sinatra::Base
-
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "hotdog"
   end
-
   get "/" do
     erb :welcome
   end
-
+  post "/signup" do
+    @trainer = Trainer.create(username: params["username"], password_digest: params["password"])
+    session[:user_id] = @trainer.id
+    redirect '/home'
+  end
+  # post '/sessions' do
+  #   @trainer = Trainer.find_by(username: params[:username], password: params[:password])
+  #   if @trainer
+  #     session[:id] = @trainer.id
+  #     redirect '/home'
+  #   end
+  #   redirect '/signup'
+  #   end
+  get '/home' do
+    @trainer = Trainer.find_by_id(session[:user_id])
+    if logged_in?
+    erb :home
+    else
+      redirect '/'
+    end
+  end
+  helpers do
+  def current_user
+    @current_user ||= Trainer.find_by_id(session[:user_id])
+  end
+  def logged_in?
+    current_user != nil
+  end
+end
 end
---
+++ db/migrate/20210104224159_create_pokedexes.rb
@@ -0,0 +1,8 @@
+class CreatePokedexes < ActiveRecord::Migration
+  def change
+    create_table :pokedexes do |t|
+      t.string :name
+      t.timestamps null: false
+    end
+  end
+end
