5ec699c53c67233b7e14342faa6d807c427f7680
+ <mcrea23@aol.com>
2021-01-05 10:34:55 -0600

    app: API working update

--- app/models/Api.rb
+++ app/models/Api.rb
@@ -3,9 +3,19 @@ class Api
   def pokemon_creator
     url = "https://pokeapi.co/api/v2/pokemon?limit=151"
     uri = URI.parse(url)
-    reponse = Net::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri)
     response.body
-
     @pokemon_data = JSON.parse(response.body)
+    @pokemon = @pokemon_data["results"].collect do |pokemon|
+      pokemon
+    end
+    @pokemon.each do |pokemon|
+      pokemons = "https://pokeapi.co/api/v2/pokemon/#{pokemon["name"]}"
+      poke_uri = URI.parse(pokemons)
+      responses = Net::HTTP.get_response(poke_uri)
+      responses.body
+      pokemon_data = JSON.parse(responses.body)
+      Pokemon.create(name: pokemon_data["name"]) 
+    end
   end
 end
