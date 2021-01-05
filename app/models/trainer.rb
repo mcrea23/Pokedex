3727678c3754684e53e66a3a79cfeb3049334ca8
+ <mcrea23@aol.com>
2021-01-05 14:29:52 -0600

    app/models/trainer: added uniqueness

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,6 @@
 class Trainer < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :username, :password_digest
+  validates_presence_of :username, :password_digest, presence: true, uniqueness: true
   has_many :pokedexes
   has_many :pokemon
 end
